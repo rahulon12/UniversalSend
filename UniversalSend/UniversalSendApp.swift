f4973adc3ef31dc477a444d583f3e91246dc711e
Rahul Narayanan <rahulon@icloud.com>
2024-03-13 11:18:19 -0400

    Initial Commit

---
+++ UniversalSend/UniversalSendApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniversalSendApp.swift
+//  UniversalSend
+//
+//  Created by Rahul on 3/13/24.
+//
+
+import SwiftUI
+
+@main
+struct UniversalSendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
